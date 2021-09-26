60844297fab4e9b1f6986d922d728e3dec224857
cb218 <66742464+cb218@users.noreply.github.com>
2021-09-26 17:17:10 -0500

    here

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Welcome home!'));
+app.get('/', (req, res) => res.send('Welcome here!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
