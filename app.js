b736a0cf529915e2343739a7c72973c59637f08b
cb218 <ec2-user@ip-172-31-84-104.ec2.internal>
2021-09-26 22:48:23 +0000

    thanks

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express()
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Welcome to my page!'));
+app.get('/', (req, res) => res.send('Welcome to my page, thank you for coming!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
